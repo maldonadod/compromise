9fcac531089b784a5380c02e0966cc762af19c87
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-20 21:38:05 -0300

    add todo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import date_header from '../components/date-header/reducer'
+import todo from '../components/todo/reducer'
 
 const rootReducer = combineReducers({
   date_header
+  ,todo
 });
 
 export default rootReducer
