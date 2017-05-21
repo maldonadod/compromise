8fe455249d4c284b5e47d94971c0061fde3fa39f
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-20 21:36:57 -0300

    add mapStateToProps on Todo

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import TodoItem from './TodoItem'
 
 class Todo extends Component {
@@ -6,11 +7,22 @@ class Todo extends Component {
   render() {
     return (
       <ul>
-        {this.props.list.map((item, index) =>
+        {this.props.goals.map((item, index) =>
           <this.props.TodoItem key={index} {...item} />)}
       </ul>
     )
   }
 }
 
-export default Todo;
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    goals: state.todo.goals
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
