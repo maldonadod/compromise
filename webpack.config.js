ae1fd729c8c24508e22ae0b9b400c6e3d066fbfb
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-23 07:40:46 -0300

    set port to 80

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-console.log(path.join(__dirname, '/src/main.js'))
+
 module.exports = {
   entry: path.join(__dirname, '/src/main'),
   module: {
