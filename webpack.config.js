2096d5609bd519c732e4ee54b0e2c247bfa4ff77
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-23 07:28:32 -0300

    add express,add jsx loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
-
+console.log(path.join(__dirname, '/src/main.js'))
 module.exports = {
-    entry: path.resolve(__dirname, 'src/main.js'),
-    output: {
-        path: path.resolve('dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
-}
+  entry: path.join(__dirname, '/src/main'),
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js','.scss', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
