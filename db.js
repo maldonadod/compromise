fe7e779a819f18345ae78c6a30f2fb017a6e7d89
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-25 22:39:57 -0300

    DB_URL

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-const url = `mongodb://dan:pQ0nYh0RpPKpPjIk@cluster0-shard-00-00-as0aw.mongodb.net:27017,cluster0-shard-00-01-as0aw.mongodb.net:27017,cluster0-shard-00-02-as0aw.mongodb.net:27017/compromise_test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
-mongoose.connect(url);
+const { DB_URL } = process.env;
+mongoose.connect(DB_URL);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
