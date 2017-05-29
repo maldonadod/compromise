ffe76a0643242b1912393326653b79b004fa3c53
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-29 20:03:19 -0300

    disabled date-header right button when got today

--- src/components/date-header/DateHeaderView.js
+++ src/components/date-header/DateHeaderView.js
@@ -10,7 +10,7 @@ const View = ({
     <div>
       <button onClick={sustractDay}>&#8592;</button>
       <span>{date.toDateString()}</span>
-      <button onClick={addDay}>&#8594;</button>
+      <button disabled={new Date(date).setHours(0,0,0,0) >= new Date().setHours(0,0,0,0)} onClick={addDay}>&#8594;</button>
     </div>
   )
 }
