55101995848f082364aa0ca3339d69917e92f808
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-05-29 20:01:02 -0300

    add cors,body-parser,handle get and post goal

--- server.js
+++ server.js
@@ -5,11 +5,46 @@ const app = express();
 env(__dirname + '/.env_file', { raise: false });
 const PORT = process.env.PORT || 80;
 const db = require('./db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
   response.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/goals', function(req, res) {
+
+  let { created_at = Date.now() } = req.query;
+  
+  var start = new Date(created_at);
+  start.setHours(0,0,0,0);
+
+  var end = new Date(created_at);
+  end.setHours(23,59,59,999);
+
+  db.Goal.find({
+    created_at: {
+      $gte: start
+      ,$lt: end
+    }
+  }, (err, goals) => {
+    res.send({
+      goals
+    });
+  });
+});
+app.post('/goals', function(req, res) {
+
+  const { body } = req;
+
+  let goal = new db.Goal(body);
+  goal.save((err, goal) => {
+    if (err) {res.send(err);}
+    res.send(goal);
+  });
 });
 
 app.listen(PORT, error => {
