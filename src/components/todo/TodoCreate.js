fcb240cb3cd0b8fe8d34e988b62249aeeb8390d5
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-06-01 21:16:45 -0300

    turn to HoC to map dispatch to props to presentational components

--- src/components/todo/TodoCreate.js
+++ src/components/todo/TodoCreate.js
@@ -2,37 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import {
   createGoalAction
+  ,updateGoalAction
 } from './actions'
-import GoalInput from '../todo-input/GoalInput'
-
-class TodoCreate extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.createGoal = this.createGoal.bind(this);
-  }
-
-  createGoal(e) {
-    if (e.keyCode == 13) {
-      const goal = e.target.value;
-      this.props.addGoal(goal)
-      e.target.value = '';
-    }
-  }
-
-  render() {
-    return (
-      <GoalInput
-        onKeyDown={this.createGoal}
-        disabled={new Date(this.props.date).setHours(0,0,0,0) < new Date().setHours(0,0,0,0)} />
-    )
-  }
-}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     addGoal: (goal) => dispatch(createGoalAction(goal))
+    ,updateGoal: (goal) => dispatch(updateGoalAction(goal))
   }
 }
 
-export default connect(null, mapDispatchToProps)(TodoCreate)
+export default View => connect(null, mapDispatchToProps)(View)
