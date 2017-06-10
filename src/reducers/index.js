7802731dec5b6881264f284e1c979b558c468f9d
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-06-10 14:50:33 -0300

    add login reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import date_header from '../components/date-header/reducer'
 import todo from '../components/todo/reducer'
+import login from '../components/login/reducer'
 
 const rootReducer = combineReducers({
   date_header
   ,todo
+  ,login
 });
 
 export default rootReducer
