dbfb4dc114b887d0bfec3419031a728f74344ed0
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-06-11 15:36:03 -0300

    add facebook login action

--- src/components/login/actions.js
+++ src/components/login/actions.js
@@ -1,10 +1,17 @@
 import {
   LOGIN_SUCCEED
+  ,FACEBOOK_LOGIN_SUCCEED
 } from './constants'
 
 export function login(user) {
   return {
     type: LOGIN_SUCCEED,
+    user
+  }
+}
+export function facebook_login(user) {
+  return {
+    type: FACEBOOK_LOGIN_SUCCEED,
     user
   }
 }
