91124537a29c26a0fd2aee65cc6d25d0e55e48c8
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-06-21 23:43:00 -0300

    remove console log

--- src/components/login/reducer.js
+++ src/components/login/reducer.js
@@ -5,7 +5,6 @@ const defaultState = {
   user: null
 }
 export default function reducer(state = defaultState, action) {
-  console.log(action)
   switch(action.type) {
     case LOGIN_SUCCEED:
       return {
