5fceeb523bfd958b551818d2ab71016d195821e5
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-06-21 23:41:39 -0300

    import react-day-picker css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "babel-core/register";
 import "babel-polyfill";
+import 'react-day-picker/lib/style.css';
 import store from './store'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
